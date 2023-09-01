da24d50b5ec7bc5a5abadcc5bed4566dd79edc2b
TJ-Shubham <shubham.c8@outlook.com>
2023-09-01 16:59:06 +0530

    make changes for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import faqQuestions from "./data";
 import "./App.css";
 import Accordion from "./Accordion";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 
 function App() {
   return (
+    <>
     <div className="app">
       <Header />
       <Accordion data={faqQuestions} />
     </div>
+    <Footer />
+    </>
   )
 }
 
