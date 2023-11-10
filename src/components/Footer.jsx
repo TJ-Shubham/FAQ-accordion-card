0b46f5715fa9233a93f4210c682737f9f8230252
TJ-Shubham <135539685+TJ-Shubham@users.noreply.github.com>
2023-11-10 18:06:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. Coded by <a href="https://github.com/TJ-Shubham"> Shubham</a>.
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. Coded by <a href="https://github.com/TJ-Shubham" target="_blank" rel="noopener noreferrer"> Shubham</a>.
      </div>
   );
 }
